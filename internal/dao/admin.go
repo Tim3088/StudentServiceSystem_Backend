ed142c48d44669e9cb29d4cb8018a6064195d0c7
prince-he <920808178@qq.com>
2024-10-04 20:39:44 +0800

    feat:新增审核标记

--- internal/dao/admin.go
+++ internal/dao/admin.go
@@ -30,6 +30,7 @@ func (d *Dao) Update(ctx context.Context, username string, name string, userType
 		"password":  password,
 	})
 }
+
 func (d *Dao) MarkFeedback(ctx context.Context, feedbackID int) error {
 	return d.orm.Model(&model.ReportFeedback{}).Create(&model.ReportFeedback{FeedbackID: feedbackID}).Error
 }
@@ -126,11 +127,30 @@ func (d *Dao) GetSpamFeedbacks(ctx context.Context) ([]map[string]interface{}, e
 			imageUrls = append(imageUrls, url)
 		}
 		res = append(res, map[string]interface{}{
-			"id":          feedback.ID,
-			"title":       feedback.Title,
-			"content":     feedback.Content,
-			"images":      imageUrls,
+			"id":      feedback.ID,
+			"title":   feedback.Title,
+			"content": feedback.Content,
+			"images":  imageUrls,
 		})
 	}
 	return res, nil
+}
+
+func (d *Dao) UpdateReceiverID(ctx context.Context, feedbackID string, receiverID int) {
+	// 使用 Updates 方法一次性更新多个字段
+	d.orm.WithContext(ctx).Model(&model.Feedback{}).Where("feedback_id = ?", feedbackID).Updates(map[string]interface{}{
+		"receiver_id": receiverID,
+	})
+}
+
+func (d *Dao) SendReplyToUser(ctx context.Context, feedbackID, message string) {
+	// 实现发送回复消息
+}
+
+func (d *Dao) ApproveSpam(ctx context.Context, feedbackID string) {
+	// 第一步
+	d.UpdateReceiverID(ctx, feedbackID, -1)
+	// 第二步
+	message := "请您在提交反馈时确保内容的有效性和准确性，感谢您的理解和配合。如有异议，请重新反馈。"
+	d.SendReplyToUser(ctx, feedbackID, message)
 }
--- internal/handler/admin/approveSpam.go
+++ internal/handler/admin/approveSpam.go
@@ -1,7 +1,41 @@
 package admin
 
-import "github.com/gin-gonic/gin"
+import (
+	"StudentServiceSystem/internal/service"
+	"StudentServiceSystem/pkg/utils"
 
+	"github.com/gin-gonic/gin"
+	"go.uber.org/zap"
+)
+
+// ApproveSpam 审核标记为垃圾反馈
 func ApproveSpam(c *gin.Context) {
-	// TODO
+	info, err := service.GetUserByUserID(c.GetInt("user_id"))
+	if err != nil {
+		zap.L().Error("获取管理员信息失败", zap.Error(err))
+		utils.JsonFail(c, 200512, "获取管理员信息失败")
+		return
+	}
+	if info.UserType != 3 {
+		utils.JsonFail(c, 200512, "当前用户不是超级管理员")
+		return
+	}
+	var request struct {
+		FeedbackID string `json:"feedback_id" binding:"required"`
+		Approval   bool   `json:"approval" binding:"required"`
+	}
+	if err := c.ShouldBindJSON(&request); err != nil {
+		utils.JsonFail(c, 200513, "请求参数错误")
+		return
+	}
+	if !request.Approval {
+		utils.JsonSuccess(c, gin.H{
+			"message": "审核已拒绝",
+		})
+		return
+	}
+	service.ApproveSpam(request.FeedbackID)
+	utils.JsonSuccess(c, gin.H{
+		"message": "反馈已成功审核标记为垃圾反馈",
+	})
 }
